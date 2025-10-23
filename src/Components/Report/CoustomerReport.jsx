40f7968a1fc191e36e6a1f4ec930dc898217764f
Md Saiful <saifulislam66info@gmail.com>
2025-10-23 16:18:04 +0600

    Implement customer report data fetching and display functionality in CoustomerReport component

--- src/Components/Report/CoustomerReport.jsx
+++ src/Components/Report/CoustomerReport.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BaseURL } from "./../../Helper/Config";
+import { getToken } from "../../Helper/SessionHelper";
+import { ToastContainer, toast } from "react-toastify";
 
 function CoustomerReport() {
+  const [customerRpData, setCustomerRpData] = useState([]);
+  const customerReportsData = async () => {
+    const res = await axios(`${BaseURL}/CoustomersReport`, {
+      headers: { token: getToken() },
+    });
+
+    if (res.data.sucess) {
+      toast.success("Customer ");
+    }
+
+    console.log("customer report data", res.data.data);
+  };
+
+  useEffect(() => {
+    customerReportsData();
+  }, []);
   return <div>CoustomerReport</div>;
 }
 
